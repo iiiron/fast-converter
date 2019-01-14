b5c7f61a2df54650ca2fd47522ca2f564e39fd3f
wanxiangming1994 <miniqueue@foxmail.com>
2019-01-14 10:24:31 +0800

    wanxm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,50 @@
+import com.alibaba.fastjson.JSON;
 import net.noboard.fastconverter.ConvertException;
+import net.noboard.fastconverter.Converter;
+import net.noboard.fastconverter.ConverterFilter;
 import net.noboard.fastconverter.filter.CommonConverterFilter;
-import net.noboard.fastconverter.handler.CommonFilterBaseConverterHandler;
-import net.noboard.fastconverter.handler.NullConverterHandler;
+import net.noboard.fastconverter.handler.*;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Map<String, Object> map = new HashMap<>();
-        int[] intary = {1,2,3,4};
-        map.put("a", intary);
+    private static class Test {
+        private boolean isSuccess;
 
-        CommonFilterBaseConverterHandler commonFilterBaseConverterHandler = new CommonFilterBaseConverterHandler(new CommonConverterFilter());
+        private Boolean isOk;
+
+        public Boolean getOk() {
+            return isOk;
+        }
+
+        public void setOk(Boolean ok) {
+            isOk = ok;
+        }
+
+        public boolean isSuccess() {
+            return isSuccess;
+        }
+
+        public void setSuccess(boolean success) {
+            isSuccess = success;
+        }
+    }
+
+    public static void main(String[] args) {
+       Test test = new Test();
+       test.setSuccess(true);
+
         try {
-            Object result = commonFilterBaseConverterHandler.convert(map);
-            System.out.println(result);
+            new BeanToMapConverterHandler(new ConverterFilter() {
+                @Override
+                public <T> Converter getConverter(T value) {
+                    return new BooleanToNumberStringConverterHandler();
+                }
+            }).convert(test);
         } catch (ConvertException e) {
             e.printStackTrace();
         }
