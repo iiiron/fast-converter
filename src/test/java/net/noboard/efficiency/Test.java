430e200243324aeb4c15e67a0a95d81957ee9bc3
wanxiangming1994 <miniqueue@foxmail.com>
2019-08-12 14:54:03 +0800

    wanxm

--- src/test/java/net/noboard/efficiency/Test.java
+++ src/test/java/net/noboard/efficiency/Test.java
@@ -14,15 +14,15 @@ public class Test {
 //        FastConverter.customDefaultConverters()
 //                .addFirst(new DateToTimeStampStringConverterHandler());
         Object o = SpeedCheck.check(a -> FastConverter.autoConvert(list));
-        SpeedCheck.check(a -> FastConverter.autoConvert(list));
-        SpeedCheck.check(a -> FastConverter.autoConvert(list));
-        SpeedCheck.check(a -> FastConverter.autoConvert(list));
-        SpeedCheck.check(a -> FastConverter.autoConvert(list));
+//        SpeedCheck.check(a -> FastConverter.autoConvert(list));
+//        SpeedCheck.check(a -> FastConverter.autoConvert(list));
+//        SpeedCheck.check(a -> FastConverter.autoConvert(list));
+//        SpeedCheck.check(a -> FastConverter.autoConvert(list));
     }
 
     private static List<BeanA> init() {
         List<BeanA> list = new ArrayList<>();
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 10000; i++) {
             BeanA beanA = new BeanA();
             beanA.setAge(i);
             beanA.setBirthday(new Date());
